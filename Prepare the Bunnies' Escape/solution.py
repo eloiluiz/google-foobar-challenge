87b4fd7a40967ccdd38d3d5179e1d7ba84d025a3
Eloi Filho <eloi.filho@electrolux.com>
2022-06-05 22:02:55 -0300

    Prepare the Bunnies' Escape challenge submitted

--- Prepare the Bunnies' Escape/solution.py
+++ Prepare the Bunnies' Escape/solution.py
@@ -18,17 +18,21 @@ __version__ = "1.0.0"
 __status__ = "Production"
 
 
-# Import libraries
+# Import Libraries
 import Queue
 
+# Configuration Parameters
+PRINT_DEBUG = False
 
 # **************************************************************
 #                           Solution
 # **************************************************************
 def solution(map):
     # Create the agent
-    agent = AStarAgent(map)
+    agent = AStarAgent(map, False, 1)
+    # Run the search
     agent.start()
+    # Return the result
     path = agent.get_path()
     return len(path)
 
@@ -49,12 +53,7 @@ class Agent:
         self._start_position = [(self._height - 1), (self._width - 1)]
         self._goal_position = [0, 0]
         self._path = []
-        # Parse the map values
-        # self._maze = []
-        # for row in range(self._height):
-        #     self._maze.append([map(int, maze[row])])
-            # for column in range(self._width):
-            #     maze[row][column] = int(maze[row][column])
+        self._path_length = 0
 
     def get_neighbors(self, coordinates=[], y=None, x=None):
         """Return the selected coordinate neighbors.
@@ -119,23 +118,32 @@ class Agent:
         return self._path
 
 class AgentSearchNode:
-    """Agent Search Methode node.
+    """Agent Search Method node.
 
-    This class represents the nodes used in the search process.
+    This class represents a node in the A* search path.
     """
 
-    def __init__(self, parent, rank, cost, position, break_wall):
+    def __init__(self, parent, rank, cost, position, path, break_wall):
         self.parent = parent
         self.rank = rank
         self.cost = cost
         self.position = position
+        self.agent_path = path
         self.break_wall = break_wall
+
+    def __hash__(self):
+      """Defines the hash method of the AgentSearchNode class.
+
+      Returns:
+          int: The object hash identification value.
+      """
+      return hash(str(self.position))
 
     def __cmp__(self, other):
         """Compare two nodes using the rank attribute as reference.
 
         Args:
-            other (A_Star_Node): The other node under comparison.
+            other (AgentSearchNode): The other node under comparison.
 
         Returns:
             int: Returns 1 if self rank is bigger, 0 if both are equal and -1 if other rank is bigger.
@@ -146,7 +154,7 @@ class AgentSearchNode:
         """Compare two nodes using the rank attribute as reference.
 
         Args:
-            other (A_Star_Node): The other node under comparison.
+            other (AgentSearchNode): The other node under comparison.
 
         Returns:
             bool: Returns True if self rank is less than other and False otherwise.
@@ -157,7 +165,7 @@ class AgentSearchNode:
         """Compare two nodes using the rank attribute as reference.
 
         Args:
-            other (A_Star_Node): The other node under comparison.
+            other (AgentSearchNode): The other node under comparison.
 
         Returns:
             bool: Returns True if self rank is less or equal than other and False otherwise.
@@ -168,7 +176,7 @@ class AgentSearchNode:
         """Compare two nodes using the rank attribute as reference.
 
         Args:
-            other (A_Star_Node): The other node under comparison.
+            other (AgentSearchNode): The other node under comparison.
 
         Returns:
             bool: Returns True if self rank is greater than other and False otherwise.
@@ -179,16 +187,15 @@ class AgentSearchNode:
         """Compare two nodes using the rank attribute as reference.
 
         Args:
-            other (A_Star_Node): The other node under comparison.
+            other (AgentSearchNode): The other node under comparison.
 
         Returns:
             bool: Returns True if self rank is greater or equal than other and False otherwise.
         """
         return (self.rank >= other.rank)
 
-
 class AStarAgent(Agent):
-    """A* Search Agent
+    """A* Search Method
 
     This class implements the A* Search algorithm.
 
@@ -199,20 +206,24 @@ class AStarAgent(Agent):
         object: The A* agent object.
     """
 
-    def __init__(self, maze):
+    def __init__(self, maze, return_first=True, break_wall=0):
         """Initialize the search agent and execute.
         """
         # Initialization process
         Agent.__init__(self, maze)
-        self._open = Queue.PriorityQueue()
-        self._closed = []
+        self._path = []
+        self._frontier = Queue.PriorityQueue()
+        self._explored = set()
+        self._return_first = return_first
+        self._break_wall = break_wall
 
     def start(self):
         """Method that starts the goal search process and returns the resulting path.
         """
         # Execute the search
-        start_node = AgentSearchNode(0, 0, self._f(self._start_position), self._start_position, 1)
-        self._search(start_node)
+        start_node = AgentSearchNode(0, 0, self._heuristics(self._start_position), self._start_position,
+                                     [self._start_position], self._break_wall)
+        return self._search(start_node)
 
     def _movement_cost(self, origin=[], destination=[]):
         """Agent heuristic function that calculates movement costs.
@@ -231,22 +242,7 @@ class AStarAgent(Agent):
         dx = abs(destination[1] - origin[1])
         return (dx + dy)
 
-    def _g(self, coordinates=[]):
-        """Agent heuristic function that calculates movement costs from the start position.
-
-        This is a heuristic function that represents the exact cost of the path from the start position to the input 
-        coordinate values. This algorithm uses the Manhattan distance as this is the standard heuristic for a square 
-        grid.
-
-        Args:
-            coordinates (list): The movement coordinates [x, y].
-
-        Returns:
-            int: The movement cost estimation.
-        """
-        return self._movement_cost(self._start_position, coordinates)
-
-    def _h(self, coordinates=[]):
+    def _heuristics(self, coordinates=[]):
         """Agent heuristic function the calculates movement costs to the goal position.
 
         This is a heuristic function that estimates the cost of the cheapest path from the input coordinate values to 
@@ -260,25 +256,11 @@ class AStarAgent(Agent):
         """
         return self._movement_cost(coordinates, self._goal_position)
 
-    def _f(self, coordinates=[]):
-        """Agent heuristic function the calculates the total cost of the selected coordinate.
-
-        This is a heuristic function that estimates the total movement cost of the input coordinate. This algorithm uses
-        the Manhattan distance as this is the standard heuristic for a square grid.
-
-        Args:
-            coordinates (list): The movement coordinates [y, x].
-
-        Returns:
-            int: The movement cost estimation.
-        """
-        return (self._g(coordinates) + self._h(coordinates))
-
     def _search(self, node):
         """Agent search method.
 
         Args:
-            node (A_Star_Node): current A* node used in the search process.
+            node (AgentSearchNode): current A* node used in the search process.
 
         Returns:
             bool: The movement result, where True means the goal position is reached and False that it hasn't.
@@ -286,71 +268,91 @@ class AStarAgent(Agent):
 
         # Rename the input node
         current_node = node
-        # Update the visited positions list
-        self._open.put(current_node)
+        # Update the frontier list (search border)
+        self._frontier.put(current_node)
 
-        # Iterate over the open queue
-        while (not self._open.empty()):
+        # Iterate over the frontier queue
+        while (not self._frontier.empty()):
 
             # Remove the lowest ranking node from the queue
-            current_node = self._open.get()
+            current_node = self._frontier.get()
             current_position = current_node.position
 
-            # Put the visited node to the closed list
-            self._closed.append(current_node)
+            # Include current node to the explored list
+            self._explored.add(current_node)
+
+            # Print current movement step
+            if (PRINT_DEBUG == True):
+                print("Current position = ", current_position)
+                for agent_position in current_node.agent_path:
+                    self._maze.mark_position(agent_position[0], agent_position[1])
+                self._maze.select_position(current_position[0], current_position[1])
+                self._maze.print_map()
+                input("PRESS ANY KEY TO CONTINUE...")
+                self._maze.clear_path()
 
             # Test for goal position
-            # If True, set path and return True
+            # If True, store the path just found (if it is shorter)
             if (self.is_goal_position(current_position[0], current_position[1])):
-                while (isinstance(current_node, AgentSearchNode)):
-                    self._path.append(current_node.position)
-                    current_node = current_node.parent
-                return True
+                new_path = current_node.agent_path
+                new_path_length = len(new_path)
+                # Check for the shorter path
+                if ((self._path_length == 0) or (new_path_length < self._path_length)):
+                    self._path = new_path
+                    self._path_length = new_path_length
+                # Check if the search must continue or stop at the first path found
+                if (self._return_first == True):
+                    return True
+                else:
+                    continue
 
             # If current position isn't the goal, search it's neighbors
             for neighbor_position in self.get_neighbors(current_position):
 
-                # First, check if the neighbor is a valid position (open path)
+                # First, check if the neighbor is a valid position (frontier path or breakable wall)
+                neighbor_break_wall = current_node.break_wall
                 neighbor_position_value = self.get_position_value(neighbor_position[0], neighbor_position[1])
-                if (current_node.break_wall > 0):
-                    current_node.break_wall -= 1
-                elif (neighbor_position_value == 1):
+                if (neighbor_position_value == 1):
+                    if (neighbor_break_wall > 0):
+                        neighbor_break_wall -= 1
+                    else:
+                        continue
+
+                # Check if neighbor is at current path already
+                is_neighbor_in_agent_path = False
+                for agent_path_position in current_node.agent_path:
+                    if (neighbor_position == agent_path_position):
+                        is_neighbor_in_agent_path = True
+                        break
+
+                # Checks if the cost of the current path is already greater than the best result found
+                neighbor_path = current_node.agent_path[:]
+                neighbor_path.append(neighbor_position)
+                neighbor_path_length = len(neighbor_path)
+                if ((self._path_length > 0) and (self._path_length < neighbor_path_length)):
+                    # Discard neighbor if it's path is too long
                     continue
 
-                # Calculate the neighbor costs (from current and from start position)
-                neighbor_new_cost = current_node.cost + self._movement_cost(current_position, neighbor_position)
-                neighbor_gcost = self._g(neighbor_position)
+                # If neighbor is not in any of the lists, add it to frontier
+                if (is_neighbor_in_agent_path == False):
+                    neighbor_new_cost = current_node.cost + self._movement_cost(current_position, neighbor_position)
+                    neighbor_rank = neighbor_new_cost + self._heuristics(neighbor_position)
+                    neighbor_node = AgentSearchNode(current_node, neighbor_rank, neighbor_new_cost, neighbor_position,
+                                                    neighbor_path, neighbor_break_wall)
+                    self._frontier.put(neighbor_node)
 
-                # Check if the neighbor is at any of the open and closed lists
-                is_neighbor_open = False
-                is_neighbor_closed = False
+            # Print current search
+            if (PRINT_DEBUG == True):
+                for explored_node in self._explored:
+                    self._maze.mark_position(explored_node.position[0], explored_node.position[1])
+                for frontier_node in self._frontier.queue:
+                    self._maze.select_position(frontier_node.position[0], frontier_node.position[1])
+                self._maze.select_position(current_position[0], current_position[1])
+                self._maze.print_map()
+                input("PRESS ANY KEY TO CONTINUE...")
+                self._maze.clear_path()
 
-                # Check if neighbor is at the open list and is a viable path
-                for open_node in self._open.queue:
-                    open_position = open_node.position
-                    if (neighbor_position == open_position):
-                        if (neighbor_new_cost < neighbor_gcost):
-                            # Remove position from the open queue
-                            self._open.queue.remove(open_node)
-                        else:
-                            is_neighbor_open = True
-
-                # Check if neighbor is at the closed list and is a viable path
-                for closed_node in self._closed:
-                    closed_position = closed_node.position
-                    if (neighbor_position == closed_position):
-                        if (neighbor_new_cost < neighbor_gcost):
-                            # Remove position from the closed list
-                            self._closed.remove(closed_node)
-                        else:
-                            is_neighbor_closed = True
-
-                # If neightbor is not in any of the lists, add it to open
-                if ((is_neighbor_open == False) and (is_neighbor_closed == False)):
-                    neighbor_node = AgentSearchNode(current_node, neighbor_new_cost + self._f(neighbor_position), neighbor_new_cost, neighbor_position, current_node.break_wall)
-                    self._open.put(neighbor_node)
-
-        # If the open list gets empty, the goal was not found
+        # If the frontier list gets empty, the goal was not found
         return False
 
 
@@ -364,6 +366,11 @@ def test():
     fail_results = 0
     # Define every test case for the application
     test_cases = [
+        [
+            [[0, 1], 
+             [0, 0]],
+            3
+        ],
         [
             [[0, 1, 1, 0], 
              [0, 0, 0, 1], 
@@ -388,6 +395,54 @@ def test():
              [0, 1, 1, 1, 1, 1], 
              [0, 0, 0, 0, 0, 0]],
             21
+        ],
+        [
+          [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+           [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+           [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+           [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+           [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+           [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+           [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+          57
+        ],
+        [
+          [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+           [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0],
+           [1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+           [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+           [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+           [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+           [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+           [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+          57
+        ],
+        [
+          [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+           [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+           [0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+           [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+           [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+           [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1],
+           [0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+           [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+           [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+           [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+           [0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 0, 0, 1, 0],
+           [0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+           [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+           [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+           [0, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+           [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+           41
         ],
     ]
     # Run every test case
